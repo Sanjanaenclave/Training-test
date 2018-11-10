64e7767c79388423a5230231ddb1c6f3aeadaebd
sruthi7 <psm7310@gmail.com>
2018-11-10 12:00:17 +0530

    sort

---
+++ Sort.java
@@ -0,0 +1,21 @@
+package task1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> l= new ArrayList<Integer>();
+		l.add(10);
+		l.add(5);
+		l.add(25);
+		l.add(45);
+		l.add(0);
+		Collections.sort(l);
+		System.out.println(l);
+
+	}
+
+}
